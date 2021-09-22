02e5a25fdb3ccaad82e5c55191c0f0a59f6d6522
Amit <amitkr24031995@gmail.com>
2021-09-22 08:38:57 +0530

    Added Employee Attendance

---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EmployeeWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constants
+            int Is_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Is_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
+        }
+    }
+}
